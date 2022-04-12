79b30b51c3c8f26e3bf2ebe46498c4105bc21b2d
DanK1368 <danielkoett@gmail.com>
2022-04-12 16:27:38 +0200

    added a logo to the page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,13 @@
 import "./Title.scss";
+import logo from "../images/logo.svg";
 
 function Title() {
   return (
-    <h1 className="title">
-      SPLI <br />
-      TTER
-    </h1>
+    <header>
+      <h1>
+        <img src={logo} alt="this is the tip calculator company logo" />
+      </h1>
+    </header>
   );
 }
 export default Title;
