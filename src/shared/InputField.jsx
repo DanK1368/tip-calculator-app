0aa94158431ba1b6e9f2fb6e136b589e5629724b
DanK1368 <danielkoett@gmail.com>
2022-04-13 10:45:32 +0200

    removed h2-tag inside label; placed title inside label

--- src/shared/InputField.jsx
+++ src/shared/InputField.jsx
@@ -2,24 +2,21 @@ import "./InputField.scss";
 
 function InputField({ className, placeholder, src, id, text, setState }) {
   const handleChange = e => {
-    setState(parseInt(e.target.value));
-    console.log(setState);
+    setState(parseFloat(e.target.value));
   };
 
   return (
     <>
-      <label htmlFor={id}>
-        <h2>{text}</h2>
-        <input
-          className={className}
-          type="number"
-          placeholder={placeholder}
-          id={id}
-          inputMode="numeric"
-          onChange={handleChange}
-        />
-        <img className="icon" src={src} alt="" />
-      </label>
+      <label htmlFor={id}>{text}</label>
+      <input
+        className={className}
+        type="number"
+        placeholder={placeholder}
+        id={id}
+        inputMode="numeric"
+        onChange={handleChange}
+      />
+      <img className="icon" src={src} alt="" />
     </>
   );
 }
