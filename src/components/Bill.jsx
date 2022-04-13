5eaaa220e8ec943e3b55cc7860fb743ba6e9d7fc
DanK1368 <danielkoett@gmail.com>
2022-04-13 11:56:35 +0200

    added label tag

--- src/components/Bill.jsx
+++ src/components/Bill.jsx
@@ -5,11 +5,11 @@ import dollarIcon from "../images/icon-dollar.svg";
 function Bill({ setBill }) {
   return (
     <div className="bill-wrapper">
+      <label htmlFor="bill">Bill</label>
       <InputField
         className="input"
         src={dollarIcon}
         id="bill"
-        text="Bill"
         setState={setBill}
       />
     </div>
