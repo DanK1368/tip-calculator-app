dd48bd6cc3047ab1a020c87fcc92be8b683cd6e0
DanK1368 <danielkoett@gmail.com>
2022-04-13 10:51:49 +0200

    button type was changed to reset instead of button

--- src/components/Calculation.jsx
+++ src/components/Calculation.jsx
@@ -13,7 +13,7 @@ function Calculation({
   setTipAmount,
   setTotal,
 }) {
-  const handleReset = () => {
+  const handleReset = e => {
     setTip(0);
     setBill(0);
     setNumOfPeople(0);
@@ -24,27 +24,29 @@ function Calculation({
   console.log(tip, bill, numOfPeople);
   return (
     <div className="calculation">
-      <div className="calculation__container">
-        <h3>
-          Tip Amount <br /> <span>/ person</span>
-        </h3>
-        {tipAmount === null ? (
-          <p className="calculation__result">$0.00</p>
-        ) : (
-          <p className="calculation__result">${tipAmount}</p>
-        )}
+      <div className="calculation__summary">
+        <div className="calculation__container">
+          <h3>
+            Tip Amount <br /> <span>/ person</span>
+          </h3>
+          {tipAmount === null ? (
+            <p className="calculation__result">$0.00</p>
+          ) : (
+            <p className="calculation__result">${tipAmount}</p>
+          )}
+        </div>
+        <div className="calculation__container">
+          <h3>
+            Total <br /> <span>/ person</span>
+          </h3>
+          {tipAmount === null ? (
+            <p className="calculation__result">$0.00</p>
+          ) : (
+            <p className="calculation__result">${total}</p>
+          )}
+        </div>
       </div>
-      <div className="calculation__container">
-        <h3>
-          Total <br /> <span>/ person</span>
-        </h3>
-        {tipAmount === null ? (
-          <p className="calculation__result">$0.00</p>
-        ) : (
-          <p className="calculation__result">${total}</p>
-        )}
-      </div>
-      <button type="button" className="reset__button" onClick={handleReset}>
+      <button type="reset" className="reset__button" onClick={handleReset}>
         RESET
       </button>
     </div>
