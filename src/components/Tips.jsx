421bf60f586c9dd3d53ab039e63247e46eb51980
DanK1368 <danielkoett@gmail.com>
2022-04-13 10:49:02 +0200

    adjustments made as radio inputs are now being used

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -6,19 +6,29 @@ function Tips({ setTip }) {
   return (
     <div className="tip-wrapper">
       <h2>Select Tips %</h2>
-      <div className="button-container">
-        <Button className="button" text="5%" value="5" setState={setTip} />
-        <Button className="button" text="10%" value="10" setState={setTip} />
-        <Button className="button" text="15%" value="15" setState={setTip} />
-        <Button className="button" text="25%" value="25" setState={setTip} />
-        <Button className="button" text="50%" value="50" setState={setTip} />
+      <fieldset className="button-container">
+        <Button className="button" text="five" value="5" setState={setTip} />
+        <Button className="button" text="ten" value="10" setState={setTip} />
+        <Button
+          className="button"
+          text="fifteen"
+          value="15"
+          setState={setTip}
+        />
+        <Button
+          className="button"
+          text="twenty five"
+          value="25"
+          setState={setTip}
+        />
+        <Button className="button" text="fifty" value="50" setState={setTip} />
         <InputField
           className="custom__tip"
           placeholder="Custom"
           id="custom"
           setState={setTip}
         />
-      </div>
+      </fieldset>
     </div>
   );
 }
